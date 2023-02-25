6aac4162dd265ac4b331e9ffc0444eb13ca58b6b
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2023-02-26 01:14:37 +0500

    SCF#1 Add mantine (#2)
    * configure github pr extension, add gitlens to recomended
    
    * add codeowners
    
    * add mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css';
+import { MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import cache from './cache';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme: 'light' }}
+        emotionCache={cache}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
